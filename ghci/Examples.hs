caaa599ab2c8debf2d8e423397a406a10997e1e9
Fabio Brau <fabio.brau@santannapisa.it>
2021-01-18 12:28:39 +0100

    Add some example

---
+++ ghci/Examples.hs
@@ -0,0 +1,12 @@
+m = Abs "x" (App (Var "x") (Var "y"))
+n = Abs "z" (App (Var "z") (Var "x"))
+
+is_free "x" m
+is_bound "x" m
+
+l2s (subs "x" m n)
+l2s (subs "y" m n)
+
+alpha_eq m n
+
+alpha_eq m (subs "x" n (Var "y"))
